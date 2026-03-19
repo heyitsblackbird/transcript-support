f9706fe9eb3463ec606118d26a5d3441a3392e07
blAcCkbiRd <sushilsharmaa4@gmail.com>
2026-03-18 23:06:45 -0400

    Added transcript summary logic

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,21 @@
 
 from fastapi import FastAPI
+from api.v1.api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title = "Transcript Summarizer API")
+
+# Connect all your routes to frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your frontend URL in production
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+app.include_router(api_router, prefix="/api/v1")
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.post("/process-transcript")
-def process_transcript(title: str, transcriptText: str):
-    # Here you can add your logic to process the transcript text and generate a summary
-    summary = f"Summary for {title}: {transcriptText[:100]}..."
-    section_summary = "Hello world!"
-    final_summary ="this is final summary"
-    key_concepts = ["concept 1", "concept 2"]
-    flashCard = [{
-                    "question": "What is the main topic of the transcript?",
-                    "answer": f"The main topic of the transcript is {title}."
-    },
-    {
-                    "question": "What are some key concepts mentioned in the transcript?",
-                    "answer": f"Some key concepts mentioned in the transcript include: {', '.join(key_concepts)}."
-    } ]  # Placeholder summary
-    return {
-        "summary": summary,
-        "section_summary": section_summary,
-        "final_summary": final_summary,
-        "key_concepts": key_concepts,
-        "flashCard": flashCard  
-    }
+def root():
+    return {"message": "Welcome to the Transcript Summarizer API!"}
