a2de91a76bcb2319a0d9dff51e82edc2e0f60ed2
blAcCkbiRd <sushilsharmaa4@gmail.com>
2026-03-20 19:59:54 -0400

    Added AI logic for transcript summarizer with gemini

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,11 @@
 from fastapi import FastAPI
 from api.v1.api import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
 
 app = FastAPI(title = "Transcript Summarizer API")
 
