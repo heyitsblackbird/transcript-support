cd743b6185eccc6e0677b28fcc1137c4329dce9e
blAcCkbiRd <sushilsharmaa4@gmail.com>
2026-04-04 22:43:15 -0400

    Added youtube URL scapping functionality.

--- backend/services/ai_service.py
+++ backend/services/ai_service.py
@@ -5,6 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from schemas.transcript import TranscriptResponse
 from core.config import OPENAI_API_KEY, GEMINI_API_KEY
 from pydantic import SecretStr
+from youtube_transcript_api import YouTubeTranscriptApi
 
 class AIService:
     def __init__(self):
@@ -14,6 +15,30 @@ class AIService:
         self.model = ChatGoogleGenerativeAI(api_key= SecretStr(GEMINI_API_KEY), model="gemini-3-flash-preview", temperature=0)
 
     async def summarize(self, transcriptText: str) -> TranscriptResponse:
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are an expert educator that summarizes transcripts and generates 7-8 high quality flashcards from the provided transcript. The flashcards should cover the most important concepts and information from the transcript. "),
+            ("user", f"Summarize the following transcript and generate flashcards:\n\n{transcriptText}")
+        ])
+
+        # Logic to follow the schema 
+        struct__llm = self.model.with_structured_output(TranscriptResponse)
+
+        # Execute the prompt and get the structured response
+        chain = prompt  | struct__llm
+        result = await chain.ainvoke({"transcriptText": transcriptText}) #type: ignore
+        return result # type: ignore
+
+    async def summarize_youtube_transcript(self, url: str) -> TranscriptResponse:
+
+        # get video id from url
+        youtube_api = YouTubeTranscriptApi()
+        
+        video_id = url.split("v=")[-1]
+        # get transcript using youtube_transcript_api
+        transcript = youtube_api.fetch(video_id)
+        # print(f"Type of transcript: {type(transcript)}")
+        transcriptText = " ".join([snippet.text for snippet in transcript])
+
         prompt = ChatPromptTemplate.from_messages([
             ("system", "You are an expert educator that summarizes transcripts and generates 7-8 high quality flashcards from the provided transcript. The flashcards should cover the most important concepts and information from the transcript. "),
             ("user", f"Summarize the following transcript and generate flashcards:\n\n{transcriptText}")
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 // Create the conection to the backend API 
 const api = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: import.meta.env.VITE_API_URL,
     headers:{
         "Content-Type": "application/json"
     }
 });
 
-const generateTranscriptSummary = async (transcript) => {
+export const generateTranscriptSummary = async (transcript) => {
     try{
         const response = await api.post("/api/v1/transcript/process-transcript", {
             transcriptText: transcript
@@ -25,5 +25,19 @@ const generateTranscriptSummary = async (transcript) => {
     }
 };
 
-
-export default generateTranscriptSummary;
+export const generateTranscriptSummaryForYouTube = async (videoUrl) => {
+    try{
+        const response = await api.post("/api/v1/transcript/process-youtube-transcript", {
+            videoUrl: videoUrl
+        });
+        console.log("API Response:", response.data);
+        return response.data;
+    }
+    catch(error){
+        if(error.response){
+            console.error("Error response:", error.response.data);
+            console.error("Status code:", error.response.status);
+        }
+        throw error;
+    }
+};
